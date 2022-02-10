e85df12536bf44a3ed127bc999ab9ad7d5141410
Dmitry Golovinov <coverkincel@Dmitrys-MacBook-Air.local>
2022-02-10 04:02:13 +0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-require('dotenv').config({path: '../config.env'})
+//require('dotenv').config({path: '../config.env'})
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect("mongodb+srv://coverkincel:200311800g@cluster0.7rzke.mongodb.net/mytable?retryWrites=true&w=majority")
     
     console.log("MongoDB connected")
 }
